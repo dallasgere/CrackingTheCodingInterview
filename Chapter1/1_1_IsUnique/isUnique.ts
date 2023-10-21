36770052189a8c4a8c126fc5914bfea2598da509
dallas <dallasgere@icloud.com>
2023-10-21 14:36:19 -0500

    changes

--- Chapter1/1_1_IsUnique/isUnique.ts
+++ Chapter1/1_1_IsUnique/isUnique.ts
@@ -1,7 +1,21 @@
-const isUnique = (s: string): boolean => {
+const isUniqueSet = (s: string): boolean => {
   let setty: Set<string> = new Set();
   for (let i = 0; i < s.length; i++) {
     if (setty.has(s[i])) {
+      return false;
+    } else if (!setty.has(s[i])) {
+      setty.add(s[i]);
+    }
+  }
+
+  return true;
+};
+
+const noSet = (s: string): boolean => {
+  let listy: Array<string> = s.split("");
+  listy.sort();
+  for (let i = 0; i < listy.length - 1; i++) {
+    if (listy[i] === listy[i + 1]) {
       return false;
     }
   }
@@ -9,4 +23,5 @@ const isUnique = (s: string): boolean => {
   return true;
 };
 
-console.log(isUnique("hello"));
+console.log(isUniqueSet("hello"));
+console.log(noSet("hello"));
